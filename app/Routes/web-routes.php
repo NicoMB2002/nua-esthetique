91b713288508741c0368dbfdd85d83c85c436bb1
NicoMB2002 <nicomoncayo@hotmail.com>
2025-11-02 14:12:13 -0500

    Added ProductsController.php, productsIndexView.php, admin footer and header and added routing for the admin pages
    Added files and adjustments for  Admin Panel

--- app/Routes/web-routes.php
+++ app/Routes/web-routes.php
@@ -6,6 +6,8 @@ declare(strict_types=1);
  * This file contains the routes for the web application.
  */
 
+use App\Controllers\ProductsController;
+use App\Controllers\DashboardController;
 use App\Controllers\HomeController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -13,6 +15,19 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 return static function (Slim\App $app): void {
 
+    //? Admin-related Routes (admin routes group)
+
+    //*Base URI: localhost/OnlineStore_Assignment2/admin/dashboard
+
+    $app -> group('/admin', function ($group) {
+        //add/ register admin routes
+        $group->get( //we use '$group' instead of $app here
+            '/dashboard', [DashboardController::class, 'index']
+        )->setName('dashboard.index');
+        $group->get(
+            '/products', [ProductsController::class, 'products']
+        )->setName('products.index');
+    });
 
     //* NOTE: Route naming pattern: [controller_name].[method_name]
     $app->get('/', [HomeController::class, 'index'])
