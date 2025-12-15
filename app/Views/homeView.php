5af59f69894d9d784eb3ecc94bd4e64ae711a5e7
Nwant <nwantolyluke@gmail.com>
2025-12-15 11:58:57 -0500

    home page update

--- app/Views/homeView.php
+++ app/Views/homeView.php
@@ -8,57 +8,52 @@ ViewHelper::loadHeader($data['title']);
 <br>
 <br>
 
-<form method="get" action="" class="form-inline my-2 my-lg-0">
+<form method="get" action="" style="float: right;" class="form-inline my-2 my-lg-0">
       <input name="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <input class="btn btn-primary my-2 my-sm-0" type="submit" value="Search">
     </form>
 
 <!--Bootstrap carousel -->
-<div id="productCarousel" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+<div id="productCarousel" class="carousel slide" style="margin-top: 50px;">
   <div class="carousel-inner" role="listbox">
 
 
 
-    <?php for ($i = 0; $i < round(count($data['products'])/3); $i++): ?>
+    <?php for ($i = 0; $i < round(count($data['products'])/6); $i++): ?>
             <?php if ($i == 0 ){?>
                 <div class="item active " data-bs-interval="4000">
             <?php }else {?>
                 <div class="item " data-bs-interval="4000">
             <?php } ?>
-    <?php for ($j = $i*3; $j < $i*3+3; $j++):
+    <?php for ($j = $i*6; $j < $i*6+6; $j++):
     if (!isset($data['products'][$j])) {
     break 2;
     }
         $product = $data['products'][$j] ?>
-            <div class="col-md-4 mb-4">
-                <div class="card h-100">
+            <div class="col-md-2 mb-4">
+                <div class="card h-100" style="width: 250px;">
                     <img
                         src="<?= hs(APP_BASE_URL.'/'.$product['path'] ?? '/images/placeholder.jpg') ?>"
                         class="card-img-top"
                         alt="<?= hs($product['name']."") ?>"
                         style="height: 200px; object-fit: cover;"
                     >
-                    <div class="card-body">
+                    <div class="card-body" style="height: 225px;">
                         <h5 class="card-title"><?= hs($product['name']."") ?></h5>
                         <p class="card-text"><?= hs(substr($product['description'], 0, 100)) ?>...</p>
                         <?php if ($product['promotion']?? 0>0): ?>
                             <p class="fw-bold text-decoration-line-through text-danger">$<?= hs(number_format($product['price'], 2)) ?></p>
                             <p class="fw-bold text-success">$<?= hs(number_format(($product['price'] * $product['promotion'] )/100, 2)) ?></p>
                             <?php else: ?>
-                             <p class="fw-bold text-success">$<?= hs(number_format($product['price'], 2)) ?></p>
+                             <p class="fw-bold text">$<?= hs(number_format($product['price'], 2)) ?></p>
                        <?php endif; ?>
                         <span class="badge bg-secondary"><?= hs($product['category_name'] ?? 'Uncategorized') ?></span>
                     </div>
-                    <div class="card-footer">
-                        <a href="/products/<?= hs($product['product_id']."") ?>" class="btn btn-primary btn-sm">View Details</a>
+                    <div class="card-footer text-bg-dark">
                         <form method="post" action="add_item" style="float:right;">
                             <input type="hidden" name="id" value='<?=$product['product_id']?>' >
-                        <input type="submit" class="btn btn-success btn-sm" value="Add">
+
+                         <input type="submit" class="btn btn-light " value="Add To Cart">
                         </form>
                     </div>
                 </div>
@@ -70,6 +65,12 @@ ViewHelper::loadHeader($data['title']);
   <?php endfor; ?>
 </div>
 
+
+ <!-- <div class="carousel-indicators" >
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>-->
       <button id="prevBtn" class="left carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
