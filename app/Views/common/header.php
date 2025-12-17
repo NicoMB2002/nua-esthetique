ba11cd16d29036d8868bdaadd5439ace004bab10
Selihom Efrem Ogbe <efremselihom1@gmail.com>
2025-12-16 22:49:20 -0500

    Localization for cart and checkOut

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Helpers\SessionManager;
 ?>
 <!DOCTYPE html>
@@ -10,10 +11,10 @@ use App\Helpers\SessionManager;
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&display=swap" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <title><?= $page_title ?></title>
 
@@ -31,119 +32,116 @@ use App\Helpers\SessionManager;
 
 
 
-<header class="navBar">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="<?= APP_BASE_URL?>/home" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <img src="<?= APP_BASE_URL?>/public/assets/resources/images/NuaLogo.png" width="200" height="200" class="me-2" />
-        </a>
-        <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link" href="<?= APP_BASE_URL?>/products"><?= trans('nav.products'); ?></a>
-        </li>
-        <li class="nav-item"><a class="nav-link" href="<?= APP_BASE_URL?>/contact"><?= trans('nav.contact'); ?></a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><?= trans('nav.faq'); ?></a></li>
-        <li class="nav-item"><a class="nav-link" href="<?= APP_BASE_URL?>/promotions"><?= trans('nav.promotions'); ?></a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><?= trans('nav.about'); ?></a></li>
-        <li class="nav-item"><a class="nav-link" href="services"><?= trans('nav.services'); ?></a></li>
-      </ul>
+    <header class="navBar">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="<?= APP_BASE_URL ?>/home" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <img src="<?= APP_BASE_URL ?>/public/assets/resources/images/NuaLogo.png" width="200" height="200" class="me-2" />
+                </a>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= APP_BASE_URL ?>/products"><?= trans('nav.products'); ?></a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="<?= APP_BASE_URL ?>/contact"><?= trans('nav.contact'); ?></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><?= trans('nav.faq'); ?></a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= APP_BASE_URL ?>/promotions"><?= trans('nav.promotions'); ?></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><?= trans('nav.about'); ?></a></li>
+                    <li class="nav-item"><a class="nav-link" href="services"><?= trans('nav.services'); ?></a></li>
+                </ul>
 
-    <div class="nav-icons">
-        <a <?php
+                <div class="nav-icons">
+                    <a <?php
 
-        if (SessionManager::has('user_id')): ?>
-            href="<?= APP_BASE_URL?>/dashboard"
-        <?php else: ?>
-            href="<?= APP_BASE_URL?>/login"
-        <?php endif; ?>><button type="button" id="accountBtn" class="btn btn-outline-dark me-2"><i class="bi bi-person-fill"></i> <?= trans('nav.account'); ?></button></a>
-        <a href="#"><button type="button" id="cartBtn"  class="btn btn-outline-dark me-2"><i class="bi bi-cart" style="color: black;"></i> <?= trans('nav.cart')?></button></a>
+                        if (SessionManager::has('user_id')): ?>
+                        href="<?= APP_BASE_URL ?>/dashboard"
+                        <?php else: ?>
+                        href="<?= APP_BASE_URL ?>/login"
+                        <?php endif; ?>><button type="button" id="accountBtn" class="btn btn-outline-dark me-2"><i class="bi bi-person-fill"></i> <?= trans('nav.account'); ?></button></a>
+                    <a href="#"><button type="button" id="cartBtn" class="btn btn-outline-dark me-2"><i class="bi bi-cart" style="color: black;"></i> <?= trans('nav.cart') ?></button></a>
 
 
-                <div class="language-switcher">
-            <?php
-            // Get current locale from global translator
-            global $translator;
-            $currentLocale = $translator->getLocale();
-            $availableLocales = $translator->getAvailableLocales();
-            ?>
+                    <div class="language-switcher">
+                        <?php
+                        // Get current locale from global translator
+                        global $translator;
+                        $currentLocale = $translator->getLocale();
+                        $availableLocales = $translator->getAvailableLocales();
+                        ?>
 
-            <?php foreach ($availableLocales as $locale): ?>
-                <?php if ($locale !== $currentLocale): ?>
-                    <a href="?lang=<?= hs($locale) ?>" class="lang-link">
-                        <?= $locale === 'en' ? 'English' : 'Français' ?>
-                    </a>
-                <?php endif; ?>
-            <?php endforeach; ?>
+                        <?php foreach ($availableLocales as $locale): ?>
+                            <?php if ($locale !== $currentLocale): ?>
+                                <a href="?lang=<?= hs($locale) ?>" class="lang-link">
+                                    <?= $locale === 'en' ? 'English' : 'Français' ?>
+                                </a>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
 
-            <span class="current-lang">
-                <?= $currentLocale === 'en' ? '🇬🇧 English' : '🇫🇷 Français' ?>
-            </span>
+                        <span class="current-lang">
+                            <?= $currentLocale === 'en' ? '🇬🇧 English' : '🇫🇷 Français' ?>
+                        </span>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    </div>
 
-      </div>
-    </div>
+        <div id="cart" class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">
 
-<div  id="cart" class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasRightLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasRightLabel">
+                </h5>
+                <button id="cartClose" type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
 
-    </h5>
-    <button id="cartClose" type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
+                <ul class="list-group">
 
-  <ul class="list-group">
+                    <?php
+                    $total = 0.0;
+                    $cart = SessionManager::get('cart');
+                    foreach ($cart ?? [] as $name => $item):
+                        $total += ($item['price'] ?? 1 * (int) $item['amount'])
+                    ?>
+                        <li class="list-group-item text-bg-dark">
+                            <?= $name ?>
+                            -
+                            <?= $item['price'] ?> $
+                            X <?= $item['amount'] ?? 0 ?>
+                            <br>
+                            <form method="post" action="remove_item">
+                                <input type="hidden" name="name" value="<?= $name ?>">
+                                <input type='submit' class="btn btn-danger" value="<?= trans('cart.delete'); ?>">
+                            </form>
+                            <form method="post" action="add_item">
+                                <input type="hidden" name="name" value='<?= $name ?>'>
+                                <input type="hidden" name="id" value='<?= $item['id'] ?>'>
+                                <input type="submit" class="btn btn-success btn-sm" value="<?= trans('cart.add'); ?>">
+                                <input style="width: 50px; height: 30px; float: right;" type="number" value="<?= $item['amount'] ?>" name="amount" class="form-range" min="0" max="20">
+                            </form>
+                        </li>
+                        <br>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+            <footer style="margin-left:20px;">
+                <h3> Total: <?= $total ?> $</h3>
+                <a class="btn btn-primary btn-sm" href="checkout"><?= trans('cart.checkOut'); ?></a>
+            </footer>
+        </div>
 
-    <?php
- $total = 0.0;
-$cart = SessionManager::get('cart');
- foreach ($cart ?? [] as $name => $item):
-    $total += ($item['price'] ?? 1 * (int) $item['amount'])
- ?>
- <li class="list-group-item text-bg-dark">
- <?= $name?>
- -
- <?= $item['price']?> $
- X <?= $item['amount']?? 0?>
- <br>
- <form method="post" action="remove_item">
-    <input type="hidden" name="name" value="<?=$name?>">
-          <input  type ='submit' class="btn btn-danger" value="Delete">
-        </form>
-    <form method="post" action="add_item">
-    <input type="hidden" name="name" value='<?=$name?>' >
-    <input type="hidden" name="id" value='<?=$item['id']?>' >
-    <input type="submit" class="btn btn-success btn-sm" value="Add">
-    <input style="width: 50px; height: 30px; float: right;" type="number" value="<?= $item['amount']?>"  name="amount" class="form-range" min="0" max="20">
-  </form>
- </li>
- <br>
-    <?php endforeach; ?>
- </ul>
-  </div>
-  <footer style="margin-left:20px;">
-    <h3 > Total: <?= $total?> $</h3>
-    <a class="btn btn-primary btn-sm" href="checkout">Checkout</a>
-  </footer>
-</div>
-
-<script>
-
-    $('#cartBtn').click(function(){
+        <script>
+            $('#cartBtn').click(function() {
 
 
-        if ($('#cart').hasClass('show')) {
-            $('#cart').removeClass('show');
-        }else{
-        $('#cart').addClass('show');
-        }
-    })
-     $('#cartClose').click(function(){
+                if ($('#cart').hasClass('show')) {
+                    $('#cart').removeClass('show');
+                } else {
+                    $('#cart').addClass('show');
+                }
+            })
+            $('#cartClose').click(function() {
 
-        $('#cart').removeClass('show');
-    })
-
-
-</script>
-</header>
+                $('#cart').removeClass('show');
+            })
+        </script>
+    </header>
--- app/Views/products.php
+++ app/Views/products.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Helpers\FlashMessage;
 use App\Helpers\ViewHelper;
 
@@ -9,73 +10,69 @@ ViewHelper::loadHeader($data['title']);
 <br>
 
 <form method="get" action="" style="float: right;" class="form-inline my-2 my-lg-0">
-      <input name="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <input class="btn btn-primary my-2 my-sm-0" type="submit" value="Search">
-    </form>
+    <input name="search" class="form-control mr-sm-2" type="search" placeholder="<?= hs(trans('products.searchBtn')); ?>" aria-label="Search">
+    <input class="btn btn-primary my-2 my-sm-0" type="submit" value="<?= hs(trans('products.searchBtn')); ?>">
+</form>
 
 <div style="margin-top: 60px;">
 
-  <?php for ($i = 0; $i < round(count($data['products'])/5); $i++): ?>
-            <div class=" mb-4">
-    <?php for ($j = $i*5; $j < $i*5+5; $j++):
-    if (!isset($data['products'][$j])) {
-    break 2;
-    }
-        $product = $data['products'][$j] ?>
-            <div class="col-md-2 mb-4">
-                <div class="card h-100" style="width: 250px;">
-                    <a href="<?= APP_BASE_URL?>/product/<?=$product['product_id']?>">
-                        <img
-                            src="<?= hs(APP_BASE_URL.'/'.$product['path'] ?? '/images/placeholder.jpg') ?>"
-                            class="card-img-top"
-                            alt="<?= hs($product['name']."") ?>"
-                            style="height: 200px; object-fit: cover;"
-                        >
-                    </a>
-                    <div class="card-body" style="height: 225px;">
-                        <h5 class="card-title"><?= hs($product['name']."") ?></h5>
-                        <p class="card-text"><?= hs(substr($product['description'], 0, 100)) ?>...</p>
-                        <?php if ($product['promotion']?? 0>0): ?>
-                            <p class="fw-bold text-decoration-line-through text-danger">$<?= hs(number_format($product['price'], 2)) ?></p>
-                            <p class="fw-bold text-success">$<?= hs(number_format(($product['price'] -($product['price'] * $product['promotion'] )/100), 2)) ?></p>
+    <?php for ($i = 0; $i < round(count($data['products']) / 5); $i++): ?>
+        <div class=" mb-4">
+            <?php for ($j = $i * 5; $j < $i * 5 + 5; $j++):
+                if (!isset($data['products'][$j])) {
+                    break 2;
+                }
+                $product = $data['products'][$j] ?>
+                <div class="col-md-2 mb-4">
+                    <div class="card h-100" style="width: 250px;">
+                        <a href="<?= APP_BASE_URL ?>/product/<?= $product['product_id'] ?>">
+                            <img
+                                src="<?= hs(APP_BASE_URL . '/' . $product['path'] ?? '/images/placeholder.jpg') ?>"
+                                class="card-img-top"
+                                alt="<?= hs($product['name'] . "") ?>"
+                                style="height: 200px; object-fit: cover;">
+                        </a>
+                        <div class="card-body" style="height: 225px;">
+                            <h5 class="card-title"><?= hs($product['name'] . "") ?></h5>
+                            <p class="card-text"><?= hs(substr($product['description'], 0, 100)) ?>...</p>
+                            <?php if ($product['promotion'] ?? 0 > 0): ?>
+                                <p class="fw-bold text-decoration-line-through text-danger">$<?= hs(number_format($product['price'], 2)) ?></p>
+                                <p class="fw-bold text-success">$<?= hs(number_format(($product['price'] - ($product['price'] * $product['promotion']) / 100), 2)) ?></p>
                             <?php else: ?>
-                             <p class="fw-bold text">$<?= hs(number_format($product['price'], 2)) ?></p>
-                       <?php endif; ?>
-                        <span class="badge bg-secondary"><?= hs($product['category_name'] ?? 'Uncategorized') ?></span>
-                    </div>
-                   <div class="card-footer bg-white text-center">
-                        <form method="post" action="add_item" class="w-100">
-                            <input type="hidden" name="id" value='<?=$product['product_id']?>'>
-                            <input type="submit" class="btn btn-light w-100 rounded-pill fw-bold border border-dark" value="Add to Cart">
-                        </form>
-                    </div>
+                                <p class="fw-bold text">$<?= hs(number_format($product['price'], 2)) ?></p>
+                            <?php endif; ?>
+                            <span class="badge bg-secondary"><?= hs($product['category_name'] ?? 'Uncategorized') ?></span>
+                        </div>
+                        <div class="card-footer bg-white text-center">
+                            <form method="post" action="add_item" class="w-100">
+                                <input type="hidden" name="id" value='<?= $product['product_id'] ?>'>
+                                <input type="submit" class="btn btn-light w-100 rounded-pill fw-bold border border-dark" value="<?= hs(trans('products.addToCartBtn')); ?>">
+                            </form>
+                        </div>
 
 
+                    </div>
                 </div>
-            </div>
 
 
+            <?php endfor; ?>
+        </div>
     <?php endfor; ?>
- </div>
-  <?php endfor; ?>
 
 
 
 </div>
 <script>
-
     $('#productCarousel').carousel();
 
-    $('.left').click(function(){
+    $('.left').click(function() {
         $('#productCarousel').carousel("prev");
     })
 
-    $('.right').click(function(){
+    $('.right').click(function() {
         $('#productCarousel').carousel("next");
 
     })
-
-
 </script>
 
 <br><br><br><br><br><br>
@@ -91,7 +88,7 @@ ViewHelper::loadHeader($data['title']);
 
 
 
-<?= FlashMessage::render()?>
+<?= FlashMessage::render() ?>
 
 <?php
 
